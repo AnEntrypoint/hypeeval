4cca60a5c4a3627fd2839eb6da526d47b2adc3f9
lanmower <almagestfraternite@gmail.com>
2023-10-03 19:09:45 +0000

    jsdoc inclusion

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
-const runCall = require( './lib/runCall.js')
-const doeval = require( './lib/eval.js')
+/**
+ * Main entry point of the application.
+ * @module index
+ */
+
+const runCall = require("./lib/runCall.js");
+const doeval = require("./lib/eval.js");
 
 module.exports = {
-    runCall,
-    doeval
-}
+  /**
+   * Function to run a call in the application.
+   * @function runCall
+   * @param {number} index - The index of the call to run.
+   * @param {Array} calls - The array of calls to run.
+   * @param {Object} input - The input object for the call.
+   * @param {string} pk - The pk value.
+   * @param {Function} ipcCall - The ipcCall function.
+   * @param {Function} refresh - The refresh function.
+   * @returns {Promise<Array>} - A promise that resolves to the updated array of calls.
+   */
+  runCall: runCall,
+
+  /**
+   * Function to evaluate JavaScript code.
+   * @function doeval
+   * @param {string} input - The JavaScript code to evaluate.
+   * @returns {Promise<Object>} - A promise that resolves to the result of the evaluation.
+   */
+  doeval: doeval
+};
